22fb8d2772f44d423ab0cde935ff439429f2671d
Jason Thompson <jason@jasont.us>
2020-07-01 14:11:06 -0400

    add event listeners

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,67 @@
-console.log('hello');
+// Event Listeners
+const newItemBtn = document.querySelector('#new-item-btn');
+const newListBtn = document.querySelector('#new-list-btn');
+const editItemBtns = document.querySelectorAll('.item-edit');
+const deleteItemBtns = document.querySelectorAll('.item-delete');
+const editListBtns = document.querySelectorAll('.list-edit');
+const deleteListBtns = document.querySelectorAll('.list-delete');
+const items = document.querySelectorAll('.item-select');
+const lists = document.querySelectorAll('.list-select');
+
+newItemBtn.addEventListener('click', createNewItem);
+newListBtn.addEventListener('click', createNewList);
+
+editItemBtns.forEach(btn => { btn.addEventListener('click', editItem) });
+deleteItemBtns.forEach(btn => { btn.addEventListener('click', deleteItem) });
+editListBtns.forEach(btn => { btn.addEventListener('click', editList) });
+deleteListBtns.forEach(btn => { btn.addEventListener('click', deleteList) });
+items.forEach(btn => { btn.addEventListener('mouseup', toggleItemDone) });
+lists.forEach(btn => { btn.addEventListener('click', openList) });
+
+// TOGGLE ITEM DONE **checkbox hover style when hover on item title??**
+function toggleItemDone() {
+  console.log('toggle-item');
+}
+
+// NEW ITEM
+function createNewItem() {
+  console.log('new-item-btn');
+}
+
+// EDIT ITEM
+function editItem() {
+  console.log('edit-item-btn');
+}
+
+// DELETE ITEM
+function deleteItem() {
+  console.log('delete-item-btn');
+}
+
+// NEW LIST
+function createNewList() {
+  console.log('new-list-btn');
+}
+
+// EDIT LIST
+function editList() {
+  console.log('edit-list-btn');
+}
+
+// DELETE LIST
+function deleteList() {
+  console.log('delete-list-btn');
+}
+
+// OPEN LIST
+function openList() {
+  console.log('list clicked');
+}
+
+// SHOW NEW ITEM FORM
+
+// SHOW NEW LIST FORM
+
+// GET TODOs
+
+// RESIZE WINDOW ??
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/js/app.js',
   mode: 'development',
+  devtool: 'source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
