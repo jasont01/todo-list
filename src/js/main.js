cf0c11a70dcbdd5b50422815f551c81da2f1f4fe
Jason Thompson <jason@jasont.us>
2020-07-01 18:58:43 -0400

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // Event Listeners
-const newItemBtn = document.querySelector('#new-item-btn');
-const newListBtn = document.querySelector('#new-list-btn');
+const newItemBtn = document.getElementById('new-item-btn');
+const newListBtn = document.getElementById('new-list-btn');
 const editItemBtns = document.querySelectorAll('.item-edit');
 const deleteItemBtns = document.querySelectorAll('.item-delete');
 const editListBtns = document.querySelectorAll('.list-edit');
@@ -8,7 +8,7 @@ const deleteListBtns = document.querySelectorAll('.list-delete');
 const items = document.querySelectorAll('.item-select');
 const lists = document.querySelectorAll('.list-select');
 
-newItemBtn.addEventListener('click', createNewItem);
+newItemBtn.addEventListener('click', showNewItemForm);
 newListBtn.addEventListener('click', createNewList);
 
 editItemBtns.forEach(btn => { btn.addEventListener('click', editItem) });
@@ -19,23 +19,57 @@ items.forEach(btn => { btn.addEventListener('mouseup', toggleItemDone) });
 lists.forEach(btn => { btn.addEventListener('click', openList) });
 
 // TOGGLE ITEM DONE **checkbox hover style when hover on item title??**
-function toggleItemDone() {
-  console.log('toggle-item');
+function toggleItemDone(e) {
+  const itemID = e.target.dataset.itemId;
+  const itemCheck = document.getElementById(`item-check${itemID}`);
+  const itemName = document.querySelector(`.item-title[data-item-id="${itemID}"]`);
+  console.log(itemName);
+  // toggle checkbox if item name was clicked
+  if (e.target.localName == 'span') itemCheck.checked = !itemCheck.checked;
+  itemName.classList.toggle('done');
+}
+
+// HandleNewItem function/module ??
+// SHOW NEW ITEM FORM
+function showNewItemForm() {
+  const addNewItemBtn = document.getElementById('new-item-add');
+  const cancelNewItemBtn = document.getElementById('new-item-cancel');
+  addNewItemBtn.addEventListener('click', createNewItem);
+  cancelNewItemBtn.addEventListener('click', hideNewItemForm);
+  //show form / modal
+  
+}
+
+// HIDE NEW ITEM FORM
+function hideNewItemForm() {
+  // hide form / modal
+
+  // clear form contents
 }
 
 // NEW ITEM
 function createNewItem() {
-  console.log('new-item-btn');
+  const itemNameField = document.getElementById('new-item-title');
+  const dateField = document.getElementById('new-item-date');
+  const priorityField = document.getElementById('new-item-priority');
+
+  //create new item
+  // itemNameField.value, dateField.value,  priorityField.value
+  // store in localstorage
+
+  hideNewItemForm();
 }
 
 // EDIT ITEM
-function editItem() {
+function editItem(e) {
   console.log('edit-item-btn');
+  console.log(e.target.dataset.itemId);
 }
 
 // DELETE ITEM
-function deleteItem() {
+function deleteItem(e) {
   console.log('delete-item-btn');
+  console.log(e.target.dataset.itemId);
 }
 
 // NEW LIST
@@ -44,21 +78,19 @@ function createNewList() {
 }
 
 // EDIT LIST
-function editList() {
-  console.log('edit-list-btn');
+function editList(e) {
+  console.log(`edit-list-btn ${e.target.dataset.listId} clicked`);
 }
 
 // DELETE LIST
-function deleteList() {
-  console.log('delete-list-btn');
+function deleteList(e) {
+  console.log(`delete-list-btn ${e.target.dataset.listId} clicked`);
 }
 
 // OPEN LIST
-function openList() {
-  console.log('list clicked');
+function openList(e) {
+  console.log(`list ${e.target.dataset.listId} clicked`);
 }
-
-// SHOW NEW ITEM FORM
 
 // SHOW NEW LIST FORM
 
