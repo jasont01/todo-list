b89bd2cea8fa8806f3496ca8484340fadc8f160a
Jason Thompson <jason@jasont.us>
2021-05-19 19:00:33 -0400

    fix refresh timing

--- src/utils/refreshToken.js
+++ src/utils/refreshToken.js
@@ -1,11 +1,15 @@
 export const refreshTokenSetup = (res, dispatch) => {
+  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+
   const refreshToken = async () => {
     const newAuthRes = await res.reloadAuthResponse();
 
+    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+
     dispatch({ type: 'refreshToken', payload: newAuthRes.id_token });
 
-    setTimeout(refreshToken, newAuthRes.expires_in);
+    setTimeout(refreshToken, refreshTiming);
   };
 
-  setTimeout(refreshToken, res.tokenObj.expires_in);
+  setTimeout(refreshToken, refreshTiming);
 };
