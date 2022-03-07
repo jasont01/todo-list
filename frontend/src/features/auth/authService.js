6d82267442c7edc458a162dd17dfb55272372bd6
Jason Thompson <jason@jasont.us>
2022-03-06 20:08:02 -0500

    WIP

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const API_URL = `${process.env.REACT_APP_API_URL}/user`
+
+const register = async (userData) => {
+  const res = await axios.post(API_URL, userData)
+  if (res.data) {
+    localStorage.setItem('user', JSON.stringify(res.data))
+  }
+  return res.data
+}
+
+const login = async (userData) => {
+  const res = await axios.post(`${API_URL}/login`, userData)
+  if (res.data) {
+    localStorage.setItem('user', JSON.stringify(res.data))
+  }
+  return res.data
+}
+
+const logout = () => {
+  localStorage.removeItem('user')
+}
+
+const authService = {
+  register,
+  login,
+  logout,
+}
+
+export default authService
---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,95 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
+
+const user = JSON.parse(localStorage.getItem('user'))
+
+const initialState = {
+  user: user ? user : null,
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  message: '',
+}
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (data, thunkAPI) => {
+    try {
+      return await authService.register(data)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+export const login = createAsyncThunk('auth/login', async (data, thunkAPI) => {
+  try {
+    return await authService.login(data)
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString()
+    thunkAPI.rejectWithValue(message)
+  }
+})
+
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout()
+})
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false
+      state.isError = false
+      state.isSuccess = false
+      state.message = ''
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null
+      })
+  },
+})
+
+export const { reset } = authSlice.actions
+export default authSlice.reducer
