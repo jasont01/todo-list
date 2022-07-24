cc2aab9c974313427d32d5759bbf1e1825f37677
Jason Thompson <jason@jasont.us>
2022-07-23 21:27:28 -0400

    add cors options

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+  origin: true,
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
